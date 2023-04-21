c2ab8eb6ef91391edf329c4713b947affe9f9980
Ramanaruban <ramanaruban29@gmail.com>
2023-04-21 15:47:28 +0530

    Add files via upload
    Practiced Testing using TestNG for Rest Assured using Eclipse IDE

---
+++ RestAssured/RestAssuredAutomation/src/test/java/tests/PutPatchDelete.java
@@ -0,0 +1,61 @@
+package tests;
+
+import static io.restassured.RestAssured.*;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+public class PutPatchDelete {
+	
+	@Test
+	public void testPut() {
+		JSONObject request = new JSONObject();
+		request.put("name", "Raghav");
+		request.put("job", "Teacher");
+		
+		baseURI = "https://reqres.in/api";
+		given().
+			header("Content-Type", "application/json"). 
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			put("/users/2").
+		then().
+			statusCode(200).
+			log().all();	
+	}
+	
+	@Test
+	public void testPatch() {
+		JSONObject request = new JSONObject();
+		request.put("name", "Raghav");
+		request.put("job", "Teacher");
+		
+		baseURI = "https://reqres.in/api";
+		
+		given().
+			header("Content-Type", "application/json"). 
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			patch("/users/2").
+		then().
+			statusCode(200).
+			log().all();		
+	}
+	
+	@Test
+	public void testDelete() {
+		baseURI = "https://reqres.in/api";
+		
+		when().
+			delete("/users/2").
+		then().
+			statusCode(204).
+			log().all();		
+	}	
+}
